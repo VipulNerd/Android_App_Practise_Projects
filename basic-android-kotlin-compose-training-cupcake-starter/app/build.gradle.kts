de0fc40819ce60d7c01556c8afa52fe0c167e458
VipulNerd <144052913+VipulNerd@users.noreply.github.com>
2025-06-16 19:08:31 +0530

    commit

--- basic-android-kotlin-compose-training-cupcake-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-cupcake-starter/app/build.gradle.kts
@@ -67,20 +67,24 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2025.03.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.compose.ui:ui-test-junit4-android:1.7.8")
+    implementation("androidx.navigation:navigation-testing:2.8.9")
+    implementation("androidx.navigation:navigation-testing-android:2.9.0-beta01")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
